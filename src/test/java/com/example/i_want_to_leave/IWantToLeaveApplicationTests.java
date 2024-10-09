39cc0ee779e11989140c1c145f08617c0df25229
chungjeongsu <wjdwlgh2000@gmail.com>
2024-10-09 22:13:14 +0900

    first commit

---
+++ src/test/java/com/example/i_want_to_leave/IWantToLeaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.i_want_to_leave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IWantToLeaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
